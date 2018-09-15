c80c6692394cfc3ae0820d94aa2beed2483ed8e4
Fernando Arellano <fernando_arellano@epam.com>
2018-09-15 13:57:19 -0500

    Practice 13. fix double import

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './../app.state';
 import * as TutorialActions from '../actions/tutorial.actions';
